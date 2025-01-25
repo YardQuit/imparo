81e1da00d79d5715fe8e63fa9364fbbfe28ae7dd
Michael Jones <yardquit@pm.me>
2025-01-25 16:03:52 +0100

    Cosmetic changes to build script

--- build.sh
+++ build.sh
@@ -7,31 +7,21 @@ RELEASE="$(rpm -E %fedora)"
 ### COPY FILES
 cp -rv /tmp/sysfiles/* /
 
-# Installs package(s)
-# dnf5 -y copr enable ublue-os/akmods
-
+### INSTALLS PACKAGE(S) FROM FEDORA REPOS
 dnf5 -y install \
 $(cat /tmp/packages/desktop_std) \
+$(cat /tmp/packages/desktop_env) \
 $(cat /tmp/packages/security) \
 $(cat /tmp/packages/fonts) \
 $(cat /tmp/packages/personal) 
 
-# Installs package(s) from copr repos
-# dnf5 -y copr enable a-zhn/ghostty
+### INSTALLS PACKAGE(S) FROM COPR REPOS
 dnf5 -y install ghostty
-# dnf5 -y copr disable a-zhn/ghostty
-
-# dnf5 -y copr enable atim/starship
 dnf5 -y install starship
-# dnf5 -y copr disable atim/starship
 
-# dnf5 -y copr enable ryanabx/cosmic-epoch
-dnf5 -y install $(cat /tmp/packages/desktop_env)
-# dnf5 -y copr disable ryanabx/cosmic-epoch
+### DISABLING SYSTEM UNIT FILE(S)
 
-### Disabling System Unit File(s)
-
-### Enabling System Unit File(s)
+### ENABLING SYSTEM UNIT FILE(S)
 systemctl enable bootc-fetch-apply-updates.timer
 systemctl enable cosmic-greeter.service
 systemctl enable tuned.service
@@ -39,14 +29,14 @@ systemctl enable podman.socket
 systemctl enable fstrim.timer
 systemctl enable firewalld.service
 
-### Change default firewalld zone
+### CHANGE DEFAULT FIREWALLD ZONE
 cp /etc/firewalld/firewalld-workstation.conf /etc/firewalld/firewalld-workstation.conf.bak
 sed -i 's/DefaultZone=FedoraWorkstation/DefaultZone=drop/g' /etc/firewalld/firewalld-workstation.conf
 
-### Fix bug preventing systemd-remount-fs.service to start - Does not work
+### FIX BUG PREVENTING SYSTEMD-REMOUNT-FS.SERVICE TO START - DOES NOT WORK
 # sed -i 's/subvol\[=.*\]/#&/g' /etc/fstab
 
-### Clean Up
+### CLEAN UP
 shopt -s extglob
 rm -rf /tmp/* || true
 rm -rf /var/!(cache)
